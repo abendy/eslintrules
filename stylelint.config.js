87aa7ded456c4a2e1177f0adb468d94efcb031a5
abendy <1480258+abendy@users.noreply.github.com>
2026-08-15 20:38:16 -0400

    fix(stylelint): codify existing theme names
    Refs abendy/amazon-docs#2

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,9 @@
 /** @type {import('stylelint').Config} */
 module.exports = {
     extends: ['stylelint-config-standard-scss'],
+    rules: {
+        'scss/dollar-variable-pattern': '^[a-z][a-zA-Z0-9]*(?:-[a-zA-Z0-9]+)*$',
+        'selector-class-pattern':
+            '^(?:[A-Z][a-zA-Z0-9]*(?:(?:__|--)[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)*|[a-z][a-zA-Z0-9-]*(?:(?:__|--)[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)*)$',
+    },
 }
